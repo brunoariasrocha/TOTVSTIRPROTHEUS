8d59729ef679fbf1ff5b90bec4501d33f63caa14
brunoariasrocha <brunoariasrocha@hotmail.com>
2025-08-20 18:14:27 -0400

    Atualizando

--- PRIMEIROTESTESUITE.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from PRIMEIROTESTECASE import PrimeiroTesteCase
-
-suite = unittest.TestSuite()
-suite.addTest(PrimeiroTesteCase('test_incluir'))
-              
-runner = unittest.TextTestRunner(verbosity=2)
-ruuner.run(suite)
---
+++ venv379/TESTE2MATA010TESTCASE.py
@@ -0,0 +1,59 @@
+from tir import Webapp
+import unittest
+
+class TESTE2MATA010(unittest.TestCase):
+
+
+	@classmethod
+	def setUpClass(inst):
+		inst.oHelper = Webapp()
+		inst.oHelper.Setup("SIGACOM","20/08/2025","01","01011001","02")
+		
+	def test_TESTE2MATA010_001(self):
+
+		descricao = "TESTE PRODUTO TIR V3"
+		
+		self.oHelper.Program("MATA010")
+		self.oHelper.SetButton("Incluir")
+		self.oHelper.WaitShow("Atualizacao de Produtos - Incluir", 20)
+		#self.oHelper.WaitFieldValue("B1_DESC", "TESTE PRODUTO TIR V3")
+		self.oHelper.SetFocus("B1_DESC") #Foco no Campo Descrição
+		self.oHelper.SetValue("B1_DESC", "")
+		self.oHelper.SetKey('Backspace')
+		self.oHelper.SetValue("B1_DESC", descricao)
+		self.oHelper.CheckHelp(text_help="Descricao", button = "Fechar")
+		self.oHelper.SetValue("B1_UM","UN")
+		self.oHelper.SetValue("B1_XAPRES","")
+		self.oHelper.SetValue("B1_XMARCA","")
+		self.oHelper.SetButton("OK")
+		self.oHelper.SetValue("B1_XMARCA","0412")
+		self.oHelper.SetValue("B1_XCAT1","07")
+		self.oHelper.SetValue("B1_XCAT2","28")
+		self.oHelper.SetValue("B1_XGRP1","000")
+		self.oHelper.SetValue("B1_XGRP2","00")
+		self.oHelper.SetValue("B1_XSKU","07280000025589")
+		self.oHelper.SetValue("B1_GRUPO","0728")
+		self.oHelper.SetValue("B1_XPERREA","0,00")
+		self.oHelper.SetValue("B1_TIPO","ME")
+		self.oHelper.SetValue("B1_LOCPAD","01")
+		self.oHelper.SetValue("B1_SEGUM","")
+		self.oHelper.SetValue("B1_CONV","0,00")
+		self.oHelper.SetKey('Tab')
+		self.oHelper.SetValue("B1_PESO","0,00000")
+		self.oHelper.SetValue("B1_PESBRU","0,00000")
+		self.oHelper.SetValue("B1_PROC","")
+		self.oHelper.SetValue("B1_LOJPROC","")
+		self.oHelper.SetValue("B1_CONTA","")
+		self.oHelper.SetValue("B1_ORIGEM","0")
+		self.oHelper.SetValue("B1_CODBAR","0000000000000")
+		self.oHelper.SetValue("B1_COD","25590")
+		self.oHelper.SetValue("B1_MSBLQL", "2 - Não")
+		self.oHelper.SetValue("B1_XCODANT","")
+		self.oHelper.SetButton("Confirmar")
+		self.oHelper.SetButton('x')
+	@classmethod
+	def tearDownClass(inst):
+		inst.oHelper.TearDown()
+
+if __name__ == '__main__':
+	unittest.main()
