2b0feb6e9e5427ff07367c3c3ef5e76f4cd8aa43
brunoariasrocha <brunoariasrocha@hotmail.com>
2025-08-21 17:48:16 -0400

    Subindo atualizações

--- venv379/TESTE2MATA010TESTCASE.py
+++ venv379/TESTE2MATA010TESTCASE.py
@@ -3,57 +3,53 @@ import unittest
 
 class TESTE2MATA010(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(inst):
+        inst.oHelper = Webapp()
+        inst.oHelper.Setup("SIGACOM","21/08/2025","01","01011001","02")
 
-	@classmethod
-	def setUpClass(inst):
-		inst.oHelper = Webapp()
-		inst.oHelper.Setup("SIGACOM","20/08/2025","01","01011001","02")
-		
-	def test_TESTE2MATA010_001(self):
+    def test_TESTE2MATA010_001(self):
 
-		descricao = "TESTE PRODUTO TIR V3"
+        self.oHelper.Program("MATA010")
+        self.oHelper.SetButton("Incluir")
+        self.oHelper.WaitShow("Atualizacao de Produtos - Incluir", 20)
+
+        # --- B1_DESC sem espaço na 1ª posição ---
+        self.oHelper.SetFocus("B1_DESC")
+        self.oHelper.SetValue(
+            "B1_DESC",
+            "PRODUTO TESTE TIR",
+            name_attr=True,
+            check_value=False
+        )
+
+        self.oHelper.SetValue("B1_UM","UN")
+        self.oHelper.SetValue("B1_XAPRES","")
+        self.oHelper.SetValue("B1_XMARCA","0412")
+        self.oHelper.SetValue("B1_XCAT1","07")
+        self.oHelper.SetValue("B1_XCAT2","28")
+        self.oHelper.SetValue("B1_XGRP1","000")
+        self.oHelper.SetValue("B1_XGRP2","00")
+        self.oHelper.SetValue("B1_GRUPO","0728")
+        self.oHelper.SetValue("B1_XPERREA","0,00")
+        self.oHelper.SetValue("B1_TIPO","ME")
+        self.oHelper.SetValue("B1_LOCPAD","01")
+        self.oHelper.SetValue("B1_SEGUM","")
+        self.oHelper.SetValue("B1_CONV","0,00")
+        self.oHelper.SetKey('Tab')
+        self.oHelper.SetValue("B1_PESO","0,00000")
+        self.oHelper.SetValue("B1_PESBRU","0,00000")
+        self.oHelper.SetValue("B1_PROC","")
+        self.oHelper.SetValue("B1_LOJPROC","")
+        self.oHelper.SetValue("B1_CONTA","")
+        self.oHelper.SetValue("B1_ORIGEM","0")
+        self.oHelper.SetValue("B1_CODBAR","0000000000000")
+        self.oHelper.SetValue("B1_MSBLQL", "2 - Não")
+        self.oHelper.SetButton("Confirmar")
 		
-		self.oHelper.Program("MATA010")
-		self.oHelper.SetButton("Incluir")
-		self.oHelper.WaitShow("Atualizacao de Produtos - Incluir", 20)
-		#self.oHelper.WaitFieldValue("B1_DESC", "TESTE PRODUTO TIR V3")
-		self.oHelper.SetFocus("B1_DESC") #Foco no Campo Descrição
-		self.oHelper.SetValue("B1_DESC", "")
-		self.oHelper.SetKey('Backspace')
-		self.oHelper.SetValue("B1_DESC", descricao)
-		self.oHelper.CheckHelp(text_help="Descricao", button = "Fechar")
-		self.oHelper.SetValue("B1_UM","UN")
-		self.oHelper.SetValue("B1_XAPRES","")
-		self.oHelper.SetValue("B1_XMARCA","")
-		self.oHelper.SetButton("OK")
-		self.oHelper.SetValue("B1_XMARCA","0412")
-		self.oHelper.SetValue("B1_XCAT1","07")
-		self.oHelper.SetValue("B1_XCAT2","28")
-		self.oHelper.SetValue("B1_XGRP1","000")
-		self.oHelper.SetValue("B1_XGRP2","00")
-		self.oHelper.SetValue("B1_XSKU","07280000025589")
-		self.oHelper.SetValue("B1_GRUPO","0728")
-		self.oHelper.SetValue("B1_XPERREA","0,00")
-		self.oHelper.SetValue("B1_TIPO","ME")
-		self.oHelper.SetValue("B1_LOCPAD","01")
-		self.oHelper.SetValue("B1_SEGUM","")
-		self.oHelper.SetValue("B1_CONV","0,00")
-		self.oHelper.SetKey('Tab')
-		self.oHelper.SetValue("B1_PESO","0,00000")
-		self.oHelper.SetValue("B1_PESBRU","0,00000")
-		self.oHelper.SetValue("B1_PROC","")
-		self.oHelper.SetValue("B1_LOJPROC","")
-		self.oHelper.SetValue("B1_CONTA","")
-		self.oHelper.SetValue("B1_ORIGEM","0")
-		self.oHelper.SetValue("B1_CODBAR","0000000000000")
-		self.oHelper.SetValue("B1_COD","25590")
-		self.oHelper.SetValue("B1_MSBLQL", "2 - Não")
-		self.oHelper.SetValue("B1_XCODANT","")
-		self.oHelper.SetButton("Confirmar")
-		self.oHelper.SetButton('x')
-	@classmethod
-	def tearDownClass(inst):
-		inst.oHelper.TearDown()
+    @classmethod
+    def tearDownClass(inst):
+        inst.oHelper.TearDown()
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
